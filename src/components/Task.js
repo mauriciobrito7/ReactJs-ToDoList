9c0bf46864595f1ff4029ea94de78f304293f71c
Mauricio <maubot01@gmail.com>
2019-08-25 15:49:47 -0400

    algunos estilos

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-
+import './Task.css';
 class Task extends Component{
+
+    styleCompleted() {
+        return {
+            fontSize: '20px',
+            color:this.props.task.done ? 'gray' : 'black', //operador ternario, si la propiedad task.done es true coloca el texto en gris sino en negro
+            textDecoration: this.props.task.done ? 'line-through' : 'none'
+        }
+    }
     
     render() {
-        /*Extraemos la propiedad props como task unicamente para poder usarla como constante */
         const {task} = this.props;
+        /*Extraemos la propiedad props como task unicamente para poder usarla como constante */
 
-        return <div>
+        return <div style= {this.styleCompleted()}>
                 { task.title } - 
                 { task.description } - 
                 { task.done } - 
                 { task.id } 
                 <input type="checkbox"></input>
-                <button>
+                <button style= {this.btnDelete}>
                     x
                 </button>
        </div>
+    }
+
+    btnDelete = {
+        fontSize:'18px',
+        background: '#ea2027',
+        color: '#fff',
+        border: 'none',
+        padding: '10px 15px',
+        borderRadius: '50%',
+        cursor:'pointer'
     }
 }
 
